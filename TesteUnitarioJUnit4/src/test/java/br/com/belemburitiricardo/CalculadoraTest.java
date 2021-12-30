5a5b4ad2e5f31d1a36559cb516b96344d2d4438f
ricardobad51 <rbbelem@gmail.com>
2021-12-29 23:22:43 -0300

    Criação mock

--- TesteUnitarioJUnit4/src/test/java/br/com/belemburitiricardo/CalculadoraTest.java
+++ TesteUnitarioJUnit4/src/test/java/br/com/belemburitiricardo/CalculadoraTest.java
@@ -1,6 +1,8 @@
 package br.com.belemburitiricardo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
@@ -10,9 +12,20 @@ public class CalculadoraTest {
 	public void testSomar() {
 		Calculadora calc = new Calculadora();
 		
-		int soma = calc.somar("1+13");
+		int soma = calc.somar("1+1+3");
 		
 		assertEquals(5, soma);
+	}
+	
+	@Test
+	public void testeSomarComMock() {
+		Calculadora calculadora = mock(Calculadora.class);
+		
+		when(calculadora.somar("1+2")).thenReturn(5);
+		
+		int resultado = calculadora.somar("1+2");
+		
+		assertEquals(5, resultado);
 	}
 
 }
--- TesteUnitarioJUnit4/src/test/java/br/com/belemburitiricardo/ExceptionTest.java
+++ TesteUnitarioJUnit4/src/test/java/br/com/belemburitiricardo/ExceptionTest.java
@@ -1,41 +1,34 @@
 package br.com.belemburitiricardo;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
 public class ExceptionTest {
-	
-	/*
-	 * @Test(expected = IndexOutOfBoundsException.class) public void empty() {
-	 * List<String> lista = new ArrayList<String>(); lista.add("Rodrigo");
-	 * 
-	 * lista.get(0); }
-	 */
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void empty() {
+		List<String> lista = new ArrayList<String>();
+		//lista.add("Rodrigo");
+
+		lista.get(0);
+	}
 
 	/*
 	 * @Rule public ExpectedException thrown = ExpectedException.none();
 	 * 
-	 * 
 	 * @Test public void shouldTestExceptionMessage() throws
-	 * IndexOutOfBoundsException{ List<Object> list = new ArrayList<Object>();
+	 * IndexOutOfBoundsException { List<Object> list = new ArrayList<Object>();
 	 * 
 	 * thrown.expect(IndexOutOfBoundsException.class);
 	 * thrown.expectMessage("Array vazio"); list.get(0); }
+	 * 
+	 * @Test public void testExceptionMessage() {
+	 * 
+	 * try { new ArrayList<Object>().get(0);
+	 * fail("Esperado que IndexOutOfBoundsException seja lançada"); } catch
+	 * (IndexOutOfBoundsException e) { assertThat(e.getMessage(),
+	 * is("Index: 0, Size: 0")); } }
 	 */
-	
-	@Test
-	public void testExceptionMessage() {
-		
-		try {
-			new ArrayList<Object>().get(0);
-			fail("Esperado que IndexOutOfBoundsException seja lançada");
-		}catch (IndexOutOfBoundsException e) {
-			assertThat(e.getMessage(), is("Index: 0, Size: 0"));
-		}
-	}
 }
