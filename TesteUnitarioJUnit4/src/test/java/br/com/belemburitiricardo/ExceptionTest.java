4bb56c4986c18cf5b979b572e705fd6d6e4fa638
ricardobad51 <rbbelem@gmail.com>
2021-12-29 22:58:12 -0300

    Criação ExpectedException

--- TesteUnitarioJUnit4/src/test/java/br/com/belemburitiricardo/ExceptionTest.java
+++ TesteUnitarioJUnit4/src/test/java/br/com/belemburitiricardo/ExceptionTest.java
@@ -1,14 +1,32 @@
 package br.com.belemburitiricardo;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class ExceptionTest {
 	
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void empty() {
-		new ArrayList<Object>().get(0);
-	}
+	/*
+	 * @Test(expected = IndexOutOfBoundsException.class) public void empty() {
+	 * List<String> lista = new ArrayList<String>(); lista.add("Rodrigo");
+	 * 
+	 * lista.get(0); }
+	 */
 
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	
+	@Test
+	public void shouldTestExceptionMessage() throws IndexOutOfBoundsException{
+		List<Object> list = new ArrayList<Object>();
+		
+		thrown.expect(IndexOutOfBoundsException.class);
+		thrown.expectMessage("Array vazio");
+		list.get(0);
+	}
 }
